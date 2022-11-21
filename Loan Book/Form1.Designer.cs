9fb596019223f38e15ba37421fe36b224cbe4e86
Desenvolvedor <Desenvolvedor@P2200042292>
2022-11-21 16:47:44 -0300

    Add project files.

---
+++ Loan Book/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace Loan_Book
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPrincipal = new System.Windows.Forms.TabControl();
+            this.tabPagePrincipal = new System.Windows.Forms.TabPage();
+            this.tabPageAuthor = new System.Windows.Forms.TabPage();
+            this.groupBoxSearchAuthor = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbAuthorNationalitySearch = new System.Windows.Forms.ComboBox();
+            this.txtAuthorNameSearch = new System.Windows.Forms.TextBox();
+            this.lblAuthorNationalitySearch = new System.Windows.Forms.Label();
+            this.lblAuthorNameSearch = new System.Windows.Forms.Label();
+            this.tabPagePublishingCompany = new System.Windows.Forms.TabPage();
+            this.tabPageTheme = new System.Windows.Forms.TabPage();
+            this.tabPageBooks = new System.Windows.Forms.TabPage();
+            this.tabPageClient = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pnlAuthor = new System.Windows.Forms.GroupBox();
+            this.dataGridAuthor = new System.Windows.Forms.DataGridView();
+            this.btnNewAuthor = new System.Windows.Forms.Button();
+            this.btnEditAuthor = new System.Windows.Forms.Button();
+            this.btnDeleteAuthor = new System.Windows.Forms.Button();
+            this.groupBoxEdit = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearchEdit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPrincipal.SuspendLayout();
+            this.tabPageAuthor.SuspendLayout();
+            this.groupBoxSearchAuthor.SuspendLayout();
+            this.tabPagePublishingCompany.SuspendLayout();
+            this.pnlAuthor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAuthor)).BeginInit();
+            this.groupBoxEdit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPrincipal
+            // 
+            this.tabPrincipal.Controls.Add(this.tabPagePrincipal);
+            this.tabPrincipal.Controls.Add(this.tabPageAuthor);
+            this.tabPrincipal.Controls.Add(this.tabPagePublishingCompany);
+            this.tabPrincipal.Controls.Add(this.tabPageTheme);
+            this.tabPrincipal.Controls.Add(this.tabPageBooks);
+            this.tabPrincipal.Controls.Add(this.tabPageClient);
+            this.tabPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tabPrincipal.Name = "tabPrincipal";
+            this.tabPrincipal.SelectedIndex = 0;
+            this.tabPrincipal.Size = new System.Drawing.Size(1216, 711);
+            this.tabPrincipal.TabIndex = 0;
+            // 
+            // tabPagePrincipal
+            // 
+            this.tabPagePrincipal.Location = new System.Drawing.Point(4, 24);
+            this.tabPagePrincipal.Name = "tabPagePrincipal";
+            this.tabPagePrincipal.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePrincipal.Size = new System.Drawing.Size(1206, 683);
+            this.tabPagePrincipal.TabIndex = 0;
+            this.tabPagePrincipal.Text = "Principal";
+            this.tabPagePrincipal.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAuthor
+            // 
+            this.tabPageAuthor.Controls.Add(this.pnlAuthor);
+            this.tabPageAuthor.Controls.Add(this.groupBoxSearchAuthor);
+            this.tabPageAuthor.Location = new System.Drawing.Point(4, 24);
+            this.tabPageAuthor.Name = "tabPageAuthor";
+            this.tabPageAuthor.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAuthor.Size = new System.Drawing.Size(1208, 683);
+            this.tabPageAuthor.TabIndex = 1;
+            this.tabPageAuthor.Text = "Autores";
+            this.tabPageAuthor.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxSearchAuthor
+            // 
+            this.groupBoxSearchAuthor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxSearchAuthor.Controls.Add(this.btnSearch);
+            this.groupBoxSearchAuthor.Controls.Add(this.cmbAuthorNationalitySearch);
+            this.groupBoxSearchAuthor.Controls.Add(this.txtAuthorNameSearch);
+            this.groupBoxSearchAuthor.Controls.Add(this.lblAuthorNationalitySearch);
+            this.groupBoxSearchAuthor.Controls.Add(this.lblAuthorNameSearch);
+            this.groupBoxSearchAuthor.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBoxSearchAuthor.Location = new System.Drawing.Point(20, 20);
+            this.groupBoxSearchAuthor.Name = "groupBoxSearchAuthor";
+            this.groupBoxSearchAuthor.Size = new System.Drawing.Size(1192, 175);
+            this.groupBoxSearchAuthor.TabIndex = 5;
+            this.groupBoxSearchAuthor.TabStop = false;
+            this.groupBoxSearchAuthor.Text = "Consulta";
+            this.groupBoxSearchAuthor.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackgroundImage = global::Loan_Book.Properties.Resources.Search;
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSearch.Location = new System.Drawing.Point(398, 47);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(50, 50);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // cmbAuthorNationalitySearch
+            // 
+            this.cmbAuthorNationalitySearch.FormattingEnabled = true;
+            this.cmbAuthorNationalitySearch.Location = new System.Drawing.Point(192, 59);
+            this.cmbAuthorNationalitySearch.Name = "cmbAuthorNationalitySearch";
+            this.cmbAuthorNationalitySearch.Size = new System.Drawing.Size(175, 29);
+            this.cmbAuthorNationalitySearch.TabIndex = 8;
+            // 
+            // txtAuthorNameSearch
+            // 
+            this.txtAuthorNameSearch.Location = new System.Drawing.Point(6, 59);
+            this.txtAuthorNameSearch.Name = "txtAuthorNameSearch";
+            this.txtAuthorNameSearch.Size = new System.Drawing.Size(175, 29);
+            this.txtAuthorNameSearch.TabIndex = 7;
+            // 
+            // lblAuthorNationalitySearch
+            // 
+            this.lblAuthorNationalitySearch.AutoSize = true;
+            this.lblAuthorNationalitySearch.Location = new System.Drawing.Point(192, 35);
+            this.lblAuthorNationalitySearch.Name = "lblAuthorNationalitySearch";
+            this.lblAuthorNationalitySearch.Size = new System.Drawing.Size(122, 21);
+            this.lblAuthorNationalitySearch.TabIndex = 6;
+            this.lblAuthorNationalitySearch.Text = "Nacionalidade";
+            this.lblAuthorNationalitySearch.Click += new System.EventHandler(this.lblAuthorNationalitySearch_Click_1);
+            // 
+            // lblAuthorNameSearch
+            // 
+            this.lblAuthorNameSearch.AutoSize = true;
+            this.lblAuthorNameSearch.Location = new System.Drawing.Point(6, 35);
+            this.lblAuthorNameSearch.Name = "lblAuthorNameSearch";
+            this.lblAuthorNameSearch.Size = new System.Drawing.Size(128, 21);
+            this.lblAuthorNameSearch.TabIndex = 5;
+            this.lblAuthorNameSearch.Text = "Nome do Autor";
+            // 
+            // tabPagePublishingCompany
+            // 
+            this.tabPagePublishingCompany.Controls.Add(this.groupBoxEdit);
+            this.tabPagePublishingCompany.Location = new System.Drawing.Point(4, 24);
+            this.tabPagePublishingCompany.Name = "tabPagePublishingCompany";
+            this.tabPagePublishingCompany.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePublishingCompany.Size = new System.Drawing.Size(1208, 683);
+            this.tabPagePublishingCompany.TabIndex = 2;
+            this.tabPagePublishingCompany.Text = "Editoras";
+            this.tabPagePublishingCompany.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTheme
+            // 
+            this.tabPageTheme.Location = new System.Drawing.Point(4, 24);
+            this.tabPageTheme.Name = "tabPageTheme";
+            this.tabPageTheme.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageTheme.Size = new System.Drawing.Size(1208, 683);
+            this.tabPageTheme.TabIndex = 3;
+            this.tabPageTheme.Text = "Temas";
+            this.tabPageTheme.UseVisualStyleBackColor = true;
+            // 
+            // tabPageBooks
+            // 
+            this.tabPageBooks.Location = new System.Drawing.Point(4, 24);
+            this.tabPageBooks.Name = "tabPageBooks";
+            this.tabPageBooks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBooks.Size = new System.Drawing.Size(1206, 683);
+            this.tabPageBooks.TabIndex = 4;
+            this.tabPageBooks.Text = "Livros";
+            this.tabPageBooks.UseVisualStyleBackColor = true;
+            // 
+            // tabPageClient
+            // 
+            this.tabPageClient.Location = new System.Drawing.Point(4, 24);
+            this.tabPageClient.Name = "tabPageClient";
+            this.tabPageClient.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageClient.Size = new System.Drawing.Size(1206, 683);
+            this.tabPageClient.TabIndex = 5;
+            this.tabPageClient.Text = "Clientes";
+            this.tabPageClient.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 689);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1216, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pnlAuthor
+            // 
+            this.pnlAuthor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlAuthor.BackColor = System.Drawing.Color.Silver;
+            this.pnlAuthor.Controls.Add(this.btnDeleteAuthor);
+            this.pnlAuthor.Controls.Add(this.btnEditAuthor);
+            this.pnlAuthor.Controls.Add(this.btnNewAuthor);
+            this.pnlAuthor.Controls.Add(this.dataGridAuthor);
+            this.pnlAuthor.Location = new System.Drawing.Point(18, 201);
+            this.pnlAuthor.Name = "pnlAuthor";
+            this.pnlAuthor.Size = new System.Drawing.Size(1190, 504);
+            this.pnlAuthor.TabIndex = 6;
+            this.pnlAuthor.TabStop = false;
+            this.pnlAuthor.Text = "groupBox1";
+            // 
+            // dataGridAuthor
+            // 
+            this.dataGridAuthor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridAuthor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridAuthor.Location = new System.Drawing.Point(6, 140);
+            this.dataGridAuthor.Name = "dataGridAuthor";
+            this.dataGridAuthor.RowTemplate.Height = 25;
+            this.dataGridAuthor.Size = new System.Drawing.Size(1178, 250);
+            this.dataGridAuthor.TabIndex = 0;
+            // 
+            // btnNewAuthor
+            // 
+            this.btnNewAuthor.BackgroundImage = global::Loan_Book.Properties.Resources.Add;
+            this.btnNewAuthor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNewAuthor.Location = new System.Drawing.Point(57, 67);
+            this.btnNewAuthor.Name = "btnNewAuthor";
+            this.btnNewAuthor.Size = new System.Drawing.Size(50, 50);
+            this.btnNewAuthor.TabIndex = 1;
+            this.btnNewAuthor.UseVisualStyleBackColor = true;
+            // 
+            // btnEditAuthor
+            // 
+            this.btnEditAuthor.BackgroundImage = global::Loan_Book.Properties.Resources.Edit;
+            this.btnEditAuthor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEditAuthor.Location = new System.Drawing.Point(165, 67);
+            this.btnEditAuthor.Name = "btnEditAuthor";
+            this.btnEditAuthor.Size = new System.Drawing.Size(50, 50);
+            this.btnEditAuthor.TabIndex = 2;
+            this.btnEditAuthor.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAuthor
+            // 
+            this.btnDeleteAuthor.BackgroundImage = global::Loan_Book.Properties.Resources.Delete;
+            this.btnDeleteAuthor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDeleteAuthor.Location = new System.Drawing.Point(265, 67);
+            this.btnDeleteAuthor.Name = "btnDeleteAuthor";
+            this.btnDeleteAuthor.Size = new System.Drawing.Size(50, 50);
+            this.btnDeleteAuthor.TabIndex = 3;
+            this.btnDeleteAuthor.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxEdit
+            // 
+            this.groupBoxEdit.Controls.Add(this.comboBox1);
+            this.groupBoxEdit.Controls.Add(this.button2);
+            this.groupBoxEdit.Controls.Add(this.btnSearchEdit);
+            this.groupBoxEdit.Controls.Add(this.label2);
+            this.groupBoxEdit.Controls.Add(this.label1);
+            this.groupBoxEdit.Location = new System.Drawing.Point(0, 6);
+            this.groupBoxEdit.Name = "groupBoxEdit";
+            this.groupBoxEdit.Size = new System.Drawing.Size(1205, 581);
+            this.groupBoxEdit.TabIndex = 0;
+            this.groupBoxEdit.TabStop = false;
+            this.groupBoxEdit.Text = "Editoras";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome da Editora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nacionalidade";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSearchEdit
+            // 
+            this.btnSearchEdit.Location = new System.Drawing.Point(324, 35);
+            this.btnSearchEdit.Name = "btnSearchEdit";
+            this.btnSearchEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchEdit.TabIndex = 2;
+            this.btnSearchEdit.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(225, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 711);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabPrincipal);
+            this.Name = "frmPrincipal";
+            this.Text = "Biblioteca";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPrincipal.ResumeLayout(false);
+            this.tabPageAuthor.ResumeLayout(false);
+            this.groupBoxSearchAuthor.ResumeLayout(false);
+            this.groupBoxSearchAuthor.PerformLayout();
+            this.tabPagePublishingCompany.ResumeLayout(false);
+            this.pnlAuthor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAuthor)).EndInit();
+            this.groupBoxEdit.ResumeLayout(false);
+            this.groupBoxEdit.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TabControl tabPrincipal;
+        private TabPage tabPagePrincipal;
+        private TabPage tabPageAuthor;
+        private TabPage tabPagePublishingCompany;
+        private TabPage tabPageTheme;
+        private TabPage tabPageBooks;
+        private TabPage tabPageClient;
+        private StatusStrip statusStrip1;
+        private GroupBox groupBoxSearchAuthor;
+        private Button btnSearch;
+        private ComboBox cmbAuthorNationalitySearch;
+        private TextBox txtAuthorNameSearch;
+        private Label lblAuthorNationalitySearch;
+        private Label lblAuthorNameSearch;
+        private GroupBox pnlAuthor;
+        private Button btnDeleteAuthor;
+        private Button btnEditAuthor;
+        private Button btnNewAuthor;
+        private DataGridView dataGridAuthor;
+        private GroupBox groupBoxEdit;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button btnSearchEdit;
+    }
+}
---
+++ Loan Book/Form1.cs
@@ -0,0 +1,40 @@
+namespace Loan_Book
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAuthorNationalitySearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAuthorNationalitySearch_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
